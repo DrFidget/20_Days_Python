794246a0d48b307224144c6de85ce741edf633d7
DrFidget <123lol45pop@gmail.com>
2023-09-19 11:12:46 +0500

    lsitQ5

---
+++ List/q4.py
@@ -0,0 +1,11 @@
+# 4. Implement a function to reverse a list.
+
+lst = [3, 4, 5, 2, 1, 2, 3, 7, 8, 9]
+
+rev = lst[::-1]
+lst.reverse()
+
+print(lst)
+
+
+print(rev)
---
+++ List/q5.py
@@ -0,0 +1,8 @@
+# 5. Write a program to find the average of elements in a list.
+lst = [1, 2, 3, 4, 5, 7, 8, 4, 3, 2, 2, 12, 0, 34]
+
+s = sum(lst)
+n = len(lst)
+
+avg = s/n
+print(avg)
